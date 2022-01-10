6b57ffb3fa3547faa91aaaa0a842e4d42693436b
null302 <44547827+null302@users.noreply.github.com>
2022-01-10 13:41:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-docker rmi -f ics_attack:latest
-docker rm ics_attack:latest
-docker build -t ics_attack:latest ./
-docker run -d -p 58087:8087 --name="ics_attack" ics_attack:latest
+docker rm -f yakit:latest
+docker build -t yakit:latest ./
+docker run -d -p 58087:8087 --name="yakit" yakit:latest
